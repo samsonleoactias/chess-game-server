e536435ae857ab7a246190d21542f8e910fe2918
Samson Actias <hello@samsonleoactias.com>
2023-10-24 16:43:35 -0400

    working, mapPieceLocationsDbToObject unfinished

--- src/controllers/utils/dbMaps/mapPieceLocationsDbToObject.ts
+++ src/controllers/utils/dbMaps/mapPieceLocationsDbToObject.ts
@@ -1,17 +1,17 @@
 import { PieceLocations } from "../../../types"
 
-const mapPieceLocationsDbToObject = (object: object) {
+const mapPieceLocationsDbToObject = (pieceLocationsDb: PieceLocationsDb) {
     const pieceLocations: PieceLocations = {
         humanPawnA: {
-            row: object.human_pawn_a_row,
-            column: object.human_pawn_a_column,
-            captured: object.human_pawn_a_captured
+            row: pieceLocationsDb.human_pawn_a_row,
+            column: pieceLocationsDb.human_pawn_a_column,
+            captured: pieceLocationsDb.human_pawn_a_captured
         },
-        
+
     };
 
     pieceLocations.humanPawnA = {
-        row: object.human_pawn_a_row,
-        column: object.human_pawn_a_column,
-        captured: object.human_pawn_a_captured
+        row: pieceLocationsDb.human_pawn_a_row,
+        column: pieceLocationsDb.human_pawn_a_column,
+        captured: pieceLocationsDb.human_pawn_a_captured
     }
