cf5a34c7065506ec463b0134abb5d75525d500df
Samson Actias <hello@samsonleoactias.com>
2024-05-02 21:37:21 -0400

    port

--- src/App.ts
+++ src/App.ts
@@ -3,7 +3,7 @@ import GameApiHandler from "./handlers/index.js";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 const { url } = await startStandaloneServer(GameApiHandler(), {
-  listen: { port: 443 },
+  listen: { port: process.env.PORT ? parseInt(process.env.PORT) : 8080 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
